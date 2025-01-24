77269f9568e85fb5126d713ac659072d5b4e2352
Waffiq Aziz <59906896+waffiqaziz@users.noreply.github.com>
2025-01-25 05:19:57 +0700

    Upgrade SDK to 35 (#28)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
   namespace = "com.waffiq.countryPicker"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.waffiq.countryPicker"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
