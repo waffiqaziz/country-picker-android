efa911dd76401b9ce5f65d4e743c28dec3c686a9
WaffiqA <59906896+waffiqaziz@users.noreply.github.com>
2026-09-25 09:33:21 +0700

    chore: increase compile sdk to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
   namespace = "com.waffiq.countryPicker"
-  compileSdk = 36
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "com.waffiq.countryPicker"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 37
     versionCode = 1
     versionName = "1.0"
 
